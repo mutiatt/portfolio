ed09b48a9230e9e9aaed1464089739d275127d33
Adenekan Mutiat <lamidemutiat@gmail.com>
2025-02-21 11:15:06 +0100

    Fixed missing react-router-dom

--- resume/vite.config.js
+++ resume/vite.config.js
@@ -5,5 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.JPG"],
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'], // Ensures Vite recognizes it
+    },
+  },
 
 })
