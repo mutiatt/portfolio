167f3610bcb3d6c4419d1ae9de89920b04f6c0e9
Adenekan Mutiat <lamidemutiat@gmail.com>
2025-02-21 11:20:50 +0100

    Fixed missing image issue

--- resume/src/Components/Navbar.jsx
+++ resume/src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../Pages/style.css";
-import logo from "../images/logo.jpg";
+import logo from "../images/logo.JPG";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
