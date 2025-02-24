94f33d6c925762cf026c3160fa3d56d936d2d35d
Adenekan Mutiat <lamidemutiat@gmail.com>
2025-02-24 11:04:39 +0100

    Fix router issue

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.JPG"],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'], // Ensures Vite recognizes it
-    },
-  },
-  resolve: {
-    alias: {
-      "react-router-dom": "react-router-dom",
-    },
-  },
-  base: '/'
-
-
-})
+  base: '/', // or '/portfolio/' if you set a subpath
+});
